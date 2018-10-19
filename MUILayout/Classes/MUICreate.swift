8189fe28c853cf92d6b14c53e816f1791366ed96
Michael Prummer <prummermichael@gmail.com>
2018-10-19 14:28:33 +0200

    btn title

--- MUILayout/Classes/MUICreate.swift
+++ MUILayout/Classes/MUICreate.swift
@@ -111,7 +111,7 @@ public class mui {
             btn.translatesAutoresizingMaskIntoConstraints = false
             btn.titleLabel?.font = fontStyle.font(size: fontSize)
             btn.contentHorizontalAlignment = alignment
-            
+            btn.setTitle(title, for: .normal)
             return btn
         }
         
